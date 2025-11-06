01cfbe92f22e1644d7f3e3cec8b9c1916566d6fa
kimjava911 <aibackend4th@gmail.com>
2025-11-06 15:40:44 +0900

    step04

--- src/main/java/org/example/usersystem/controller/ContentsController.java
+++ src/main/java/org/example/usersystem/controller/ContentsController.java
@@ -15,8 +15,12 @@ public class ContentsController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String path = req.getRequestURI(); // /content/hello
         System.out.println(path);
-        String value = path.split("/")[2];
-        req.setAttribute("value", value);
+        try {
+            String value = path.split("/")[2];
+            req.setAttribute("value", value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         req.getRequestDispatcher("/WEB-INF/views/contents.jsp").forward(req, resp);
     }
 }
